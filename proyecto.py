524e6c6a45d1e9b93413cd0706f2cb31176fd411
luismario07hr <20255813@esen.edu.sv>
2025-11-17 15:58:08 -0600

    Funciones
    En este cambio, se creo un archivo de modulo en el cual se estaran agrengando las dunciones que serviran para que las funciones de el programa funcionen

--- proyecto.py
+++ proyecto.py
@@ -1,4 +1,4 @@
-from moduloproductos import ingresar_productos
+from moduloproductos import ingresar_productos, mostras_prodcutos, eliminar_productos
 
 productos = []
 productos_eliminados = []
@@ -6,7 +6,7 @@ productos_eliminados = []
 numcode = 1
 
 estado = True
-while True: 
+while estado: 
     opcion = input("""(1: Agregar producto)
 (2: Mostrar productos)
 (3: Cambiar cantidad producto)
@@ -19,11 +19,11 @@ Elija la opción: """)
     if opcion == "7": 
         estado = False
     elif opcion == "1": 
-        ingresar_productos(productos)
+        ingresar_productos(productos, numcode)
     elif opcion == "2": 
-        print (productos)
+        mostras_prodcutos (productos)
     elif opcion == "3": 
-        x = 0 
+        eliminar_productos (productos, productos_eliminados)
     elif opcion == "4": 
         x = 0
     elif opcion == "5": 
