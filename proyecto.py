f9b2e7872d99601fccddada5174e4e0821e549f7
luismario07hr <20255813@esen.edu.sv>
2025-11-19 15:44:12 -0600

    Comentarios y funcion agregar producto y productos eliminado
    Se agregaron comentarios explicando lo que hace cada parte del menú y de las funciones. Hay dos funciones nuevas. Una para mostrar los productos que se han eliminado y otro para agregar productos (cuando se hacen pedidos a los mayoristas).

--- proyecto.py
+++ proyecto.py
@@ -1,11 +1,15 @@
-from moduloproductos import ingresar_productos, mostras_prodcutos, eliminar_productos, cantidad_producto, mostrar_estante
+from moduloproductos import ingresar_productos, mostrar_productos, eliminar_productos, cantidad_producto, mostrar_estante, producto_eliminado, agregar_producto
 from modulopedidos import colgar_pedido, mostrar_pedidos
 
+#Listas donde se agregaran los datos (diccionarios)
 productos = []
 productos_eliminados = []
 pedidos = []
+
+#Contador
 numcode = 0
 
+#Mensaje de bienvenida para el usuario
 inicio = input ("""Bienvenido Usuario, gracias por usar nuestro
 programa. Este está diseñado para que llevar el inventario
 de tus productos no sea un dolor de cabeza :).  Aquí, podrás
@@ -14,6 +18,8 @@ El programa se encargará de actualizar todo de manera automática.
 Para continuar, envía cualquier caracter: """)
 
 estado = True
+
+#Menu donde se realizaran todas las acciones
 while estado: 
     opcion = input("""(1: Agregar producto)
 (2: Mostrar productos)
@@ -22,29 +28,35 @@ while estado:
 (5: Colgar pedido)
 (6: Mostrar pedidos) 
 (7: Mostrar productos en el estante)
-(8: Mostrar pronóstico de la demanda del producto)
-(9: Calcular el error)
-(10: Salir)
+(8: Mostrar productos eliminados)
+(9: Mostrar pronóstico de la demanda del producto)
+(10: Calcular el error)
+(11: Agregar producto en stock)
+(12: Salir)
 Elija la opción (número): """)
  
-    if opcion == "10":
+    if opcion == "12": #Salir del programa
         print ("Gracias por usar nuestro sistema!!!!!!!!!!!")
         estado = False
-    elif opcion == "1": 
+    elif opcion == "1": #Ingresar productos
         numcode = ingresar_productos(productos, numcode)
-    elif opcion == "2": 
-        mostras_prodcutos (productos)
-    elif opcion == "3": 
+    elif opcion == "2": #Mostrar todos los productos disponibles
+        mostrar_productos (productos)
+    elif opcion == "3": #Mostrar la cantidad que hay de un producto
         cantidad_producto (productos)
-    elif opcion == "4":
+    elif opcion == "4": #Eliminar productos
         eliminar_productos (productos, productos_eliminados)
-    elif opcion == "5": 
+    elif opcion == "5": #Colgar pedidos
         colgar_pedido(pedidos, productos)
-    elif opcion == "6": 
+    elif opcion == "6": #Mostrar todos los pedidos
         mostrar_pedidos(pedidos)
-    elif opcion == "7":
+    elif opcion == "7": #Mostrar estante
         mostrar_estante(productos)
     elif opcion == "8":
-        x = 0  
+        producto_eliminado(productos_eliminados) #Mostrar los productos que se eliminan
     elif opcion == "9": 
         x = 0
+    elif opcion == "10": 
+        x = 0
+    elif opcion == "11": 
+        agregar_producto(productos)
