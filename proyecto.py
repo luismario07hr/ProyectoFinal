9e923efa7ab36e705713af523728df85a6961c20
Gerardo0079 <20255852@esen.edu.sv>
2025-11-19 01:01:29 -0600

    Funciones para pronósticos
    Se crearon tres funciones: Ingresar datos mensuales (demanda), calcular los pronósticos en base a la demanda (los dos tipos: Promedio móvil simple y Suavización exponencial). Además, se añadió al modulo proyecto las importaciones necesarias para que funcione correctamente, junto con los añadidos del sub menú y poder probar con distintos tipos de alpha y de n. Por último, una pequeña pausa con enter para que el usuario tenga más tiempo para leer los resultados en la terminal

--- proyecto.py
+++ proyecto.py
@@ -1,10 +1,19 @@
 from moduloproductos import ingresar_productos, mostras_prodcutos, eliminar_productos, cantidad_producto 
-from modulopedidos import colgar_pedido, mostrar_pedidos
+from modulopedidos import colgar_pedido #, mostrar_pedidos
+from modulopronosticos import ingresar_datos_mensuales, calcular_pronosticos, calcular_error_modelo
 
 productos = []
 productos_eliminados = []
 pedidos = []
 numcode = 0
+
+# Diccionario para guardar datos del pronóstico (demandas, n, alpha)
+datos_pronostico = {
+    "demandas": [],
+    "n": 0,
+    "alpha": 0.0,
+    "listo_para_error": False
+}
 
 estado = True
 while estado: 
@@ -35,6 +44,20 @@ Elija la opción: """)
     elif opcion == "6": 
         mostrar_pedidos(pedidos)
     elif opcion == "7":
-        x = 0
+    # Sub-menú para elegir entre datos nuevos o anteriores 
+        print("1. Ingresar nuevos datos históricos")
+        print("2. Usar datos existentes para calcular")
+        sub_opcion = input("Seleccione (1-2): ")
+        
+        if sub_opcion == "1":
+            ingresar_datos_mensuales(datos_pronostico)
+            calcular_pronosticos(datos_pronostico)
+        elif sub_opcion == "2":
+            calcular_pronosticos(datos_pronostico)
+        else: 
+            print ("Opción no válida, intente de nuevo pls")
+            
     elif opcion == "8":
-        x = 0  
+        calcular_error_modelo(datos_pronostico)
+        
+    pausa = input ("Preione enter para continuar. . . ")
